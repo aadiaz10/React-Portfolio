279fc244f03a475178784cbe58e10c0b8c7bf759
Alex <aadiaz10gmail.com>
2024-09-11 03:40:15 -0500

    added code

---
+++ public/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <a href="https://github.com/yourgithub" target="_blank" rel="noopener noreferrer">GitHub</a> |{' '}
+        <a href="https://linkedin.com/in/yourlinkedin" target="_blank" rel="noopener noreferrer">LinkedIn</a> |{' '}
+        <a href="https://twitter.com/yourtwitter" target="_blank" rel="noopener noreferrer">Twitter</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
